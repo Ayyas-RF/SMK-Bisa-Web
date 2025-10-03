ca2c5cfd45dfda6ee074ce4325bb462495cea7bf
Ayyas-RF <berrzert9@gmail.com>
2025-10-03 20:59:23 +0700

    Remove unused 'SimpleApp.jsx'

--- smk-bisa-website/src/main.jsx
+++ smk-bisa-website/src/main.jsx
@@ -1,18 +1,13 @@
-import { StrictMode, useEffect } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import SimpleApp from './SimpleApp.jsx'
-
-// Use SimpleApp if you want to see all content without animations
-// Change App to SimpleApp below if content is not showing
-const AppToUse = App;
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
 // Mark document as JS-enabled early to ensure CSS reveal works
-document.documentElement.classList.add('js-enabled')
+document.documentElement.classList.add("js-enabled");
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppToUse />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
